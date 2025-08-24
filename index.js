1d701ae22dbacf6473badd3c692d4e8899e7ff4a
Samuel Horner <samkoseyhorner@gmail.com>
2025-08-24 12:07:11 +0100

    Added blog and wrote from-c-to-zig

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const html = document.querySelector("html");
 
 // Theme
-const darkModeIcon = "images/dark-mode.svg"
-const lightModeIcon = "images/light-mode.svg"
-const themeIcon = document.getElementById("theme-toggle")
+const darkModeIcon = "/images/dark-mode.svg";
+const lightModeIcon = "/images/light-mode.svg";
+const themeIcon = document.getElementById("theme-toggle");
 
 let theme = localStorage.getItem("theme");
-if (!theme){
+if (!theme) {
     theme = "light";
     localStorage.setItem("theme", "light");
 }
@@ -29,25 +29,28 @@ function setTheme() {
 function darkMode() {
     html.setAttribute("data-theme", "dark");
     themeIcon.src = darkModeIcon;
-    localStorage.setItem("theme", "dark")
+    localStorage.setItem("theme", "dark");
 }
 
 function lightMode() {
     html.setAttribute("data-theme", "light");
     themeIcon.src = lightModeIcon;
-    localStorage.setItem("theme", "light")
+    localStorage.setItem("theme", "light");
 }
 
 // Progress Bar
 // https://www.w3schools.com/howto/howto_js_scroll_indicator.asp
 const progressBar = document.getElementById("progress-bar");
 const scroller = document.getElementById("content");
-scroller.onscroll = function () { progressBarUpdate() };
+scroller.onscroll = function () {
+    progressBarUpdate();
+};
 
 function progressBarUpdate() {
-    let height = scroller.clientHeight;
-    let scrollHeight = scroller.scrollHeight - height;
-    let scrollTop = scroller.scrollTop;
-    let percent = scrollTop / scrollHeight * 100;
+    const height = scroller.clientHeight;
+    const scrollHeight = scroller.scrollHeight - height;
+    const scrollTop = scroller.scrollTop;
+    const percent = scrollTop / scrollHeight * 100;
     progressBar.style.flexBasis = Math.min(percent, 100) + "%";
-} 
+}
+
